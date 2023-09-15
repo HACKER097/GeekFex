ca85bdcaf5a718401b35428dabb37ac3ded27737
mulitate4 <aryan.dongre1605@gmail.com>
2023-09-15 14:08:21 +0530

    restructured again

---
+++ frontend/website/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='bg-black'>
+
+      <h1 className='text-red-700'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
